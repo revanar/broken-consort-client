a6dab1b6fa758f45c16d5f0b85a26bbeb2fcc77d
mmatt <m.matt.barry@gmail.com>
2016-10-04 10:17:21 -0400

    added basic acceptance tests, generated about route

--- tests/acceptance/app-test.js
+++ tests/acceptance/app-test.js
@@ -10,3 +10,42 @@ test('visiting index', function(assert) {
     assert.equal(currentURL(), '/');
   });
 });
+
+test('should redirect to songs route', function(assert){
+  visit('/');
+  assert.equal(currentURL(), '/songs', 'should redirect automatically');
+});
+
+test('should list all transcribed songs', function(assert){
+  visit('/');
+  assert.equal(find('.listing').length, 3, 'should see 3 listings');
+});
+
+test('should link to list of books', function(assert){
+  visit('/');
+  click('a:contains("Books")');
+  andThen(function(){
+    assert.equal(currentURL(), '/books', 'should navigate to books');
+  });
+});
+
+test('should link to list of collections', function(assert){
+  visit('/');
+  click('a:contains("Collections")');
+  andThen(function(){
+    assert.equal(currentURL(), '/collections', 'should navigate to colletions');
+  });
+});
+
+test('should link to information about project', function(assert){
+  visit('/');
+  click('a:contains("About")');
+  andThen(function(){
+    assert.equal(currentURL(), '/about', 'should navigate to about');
+  })
+});
+
+// additional tests to add:
+//   checking that each different type of filter works on songs
+//   potentially adding details pages to books/collections
+//   adding a lightbox component to add/edit tags
