402b6ca466cbfbe2e106302b92722cd5ad397e4f
mmatt <m.matt.barry@gmail.com>
2016-10-18 17:59:55 -0400

    replaced songs table with ember-models-table implimentation

--- app/routes/songs.js
+++ app/routes/songs.js
@@ -4,4 +4,5 @@ export default Ember.Route.extend({
   model() {
     return this.get('store').query('song', {include: 'book,book.editor,composer,languages,tags', sort:'book.name,song_no'});
   }
+
 });
